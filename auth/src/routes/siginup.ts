3364e99a2ea5c4b81fc1bc2883c5d289f2c4c03c
besarkutleshi <besar.kutleshi@keds-energy.com>
2022-04-28 15:27:16 +0200

    Sign in process finished

--- auth/src/routes/siginup.ts
+++ auth/src/routes/siginup.ts
@@ -1,10 +1,9 @@
 import express, { Request, Response } from 'express'
-import { body, validationResult } from 'express-validator';
+import { body } from 'express-validator';
 import { BadRequestError } from '../errors/bad-request-error';
-import { RequestValidatorError } from '../errors/request-validation-error';
 import { User } from '../models/user';
 import jwt from 'jsonwebtoken'
-import req from 'cookie-session'
+import { validateRequest } from '../middlewares/validate-request';
 const router = express.Router();
 
 router.post('/api/users/signup', 
@@ -12,11 +11,8 @@ router.post('/api/users/signup',
         body('email').isEmail().withMessage("Email must be valid!"),
         body('password').trim().isLength({min:4, max:20}).withMessage("Password must be between 4 and 20 characters!")
     ], 
+    validateRequest,
     async (req: Request, res: Response) => {
-        const errors = validationResult(req);
-        if(!errors.isEmpty()){
-            throw new RequestValidatorError(errors.array(), "Invalid email or password!");
-        }
         const { email, password } = req.body;
         const existingUser = await User.findOne({ email });
         if(existingUser){
--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,33 @@
-import express from 'express'
-
+import express, { Request, Response } from 'express'
+import { body } from 'express-validator';
+import { BadRequestError } from '../errors/bad-request-error';
+import { validateRequest } from '../middlewares/validate-request';
+import { User } from '../models/user';
+import jwt from 'jsonwebtoken'
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {
-    res.send("Hi there!");
-});
+router.post('/api/users/signin',
+    [
+
+        body('email').isEmail().withMessage("Email must be valid!"),
+        body('password').trim().notEmpty().withMessage("You must suppy a password!")
+    ],
+    validateRequest,
+    async (req: Request, res: Response) => {
+        const { email, password } = req.body;
+        const existsUser = await User.findOne({ email, password });
+        if(!existsUser){
+            throw new BadRequestError('Invalid email or password!');
+        }
+        const userJwt = jwt.sign({
+            id: existsUser.id,
+            email: existsUser.email
+        }, process.env.JWT_KEY!);
+        req.session = {
+            jwt: userJwt
+        };
+        return res.status(200).send(existsUser);
+    }
+);
 
 export { router as signInRouter };
